9bda21e6a5564be8ba4abdf7fd298c49959c9df7
NoHaxito <santifeb2034@gmail.com>
2024-04-22 01:26:38 +0000

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,7 @@
-sudo apt-get install neovim
-git clone -b nvim-dotfiles https://github.com/NoHaxito/dotfiles ~/.config/nvim && nvim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux64.tar.gz
+echo "export PATH="$PATH:/opt/nvim-linux64/bin"" | tee -a ~/.bashrc
+git clone -b nvim-dotfiles https://github.com/NoHaxito/dotfiles ~/.config/nvim 
+rm -rf .git
+rm -r ./install.sh
